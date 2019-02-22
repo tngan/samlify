f59bd2ac20980e0e716866136a7de018cf56e5d4
step4 <c.rausch020@gmail.com>
2019-02-22 17:16:44 +0100

    node-xmllint as validator added (#239)
    * node-xmllint as validator added
    
    * Fix the emscripten loading and avoid reading files when it validates everytime

--- src/schema-validator.ts
+++ src/schema-validator.ts
@@ -3,7 +3,8 @@ import * as path from 'path';
 
 enum SchemaValidators {
   JAVAC = '@passify/xsd-schema-validator',
-  LIBXML = 'libxml-xsd'
+  LIBXML = 'libxml-xsd',
+  XMLLINT = 'node-xmllint'
 }
 
 interface SchemaValidator {
@@ -23,7 +24,9 @@ const moduleResolver = (name: string) => {
 
 const getValidatorModule: GetValidatorModuleSpec = async () => {
 
-  const selectedValidator: string = moduleResolver(SchemaValidators.JAVAC) || moduleResolver(SchemaValidators.LIBXML);
+  const selectedValidator: string = moduleResolver(SchemaValidators.JAVAC)
+    || moduleResolver(SchemaValidators.LIBXML)
+    || moduleResolver(SchemaValidators.XMLLINT);
 
   const xsd = 'saml-schema-protocol-2.0.xsd';
 
@@ -52,7 +55,7 @@ const getValidatorModule: GetValidatorModuleSpec = async () => {
       return v;
     };
 
-    const validator = await import (SchemaValidators.JAVAC);
+    const validator = await import(SchemaValidators.JAVAC);
     const mod = setSchemaDir(new validator());
 
     return {
@@ -74,7 +77,7 @@ const getValidatorModule: GetValidatorModuleSpec = async () => {
   }
 
   if (selectedValidator === SchemaValidators.LIBXML) {
-    const mod = await import (SchemaValidators.LIBXML);
+    const mod = await import(SchemaValidators.LIBXML);
     return {
       validate: (xml: string) => {
         return new Promise((resolve, reject) => {
@@ -93,6 +96,46 @@ const getValidatorModule: GetValidatorModuleSpec = async () => {
               return resolve('SUCCESS_VALIDATE_XML');
             });
           });
+        });
+      }
+    };
+  }
+
+  if (selectedValidator === SchemaValidators.XMLLINT) {
+
+    const mod = await import(SchemaValidators.XMLLINT);
+
+    const schemaPath = (schema: string) => path.resolve(__dirname, `../schemas/${schema}`);
+
+    let schemaProto = fs.readFileSync(schemaPath(xsd), 'utf-8');
+    let schemaAssert = fs.readFileSync(schemaPath('saml-schema-assertion-2.0.xsd'), 'utf-8');
+    let schemaXmldsig = fs.readFileSync(schemaPath('xmldsig-core-schema.xsd'), 'utf-8');
+    let schemaXenc = fs.readFileSync(schemaPath('xenc-schema.xsd'), 'utf-8');
+
+    // file fix for virtual filesystem of emscripten
+    schemaProto = schemaProto.replace('saml-schema-assertion-2.0.xsd', 'file_0.xsd');
+    schemaProto = schemaProto.replace('xmldsig-core-schema.xsd', 'file_1.xsd');
+    schemaAssert = schemaAssert.replace('xmldsig-core-schema.xsd', 'file_1.xsd');
+    schemaAssert = schemaAssert.replace('xenc-schema.xsd', 'file_2.xsd');
+    schemaXenc = schemaXenc.replace('xmldsig-core-schema.xsd', 'file_1.xsd');
+
+    return {
+      validate: (xml: string) => {
+
+        return new Promise((resolve, reject) => {
+
+          const validationResult = mod.validateXML({
+            xml: xml,
+            schema: [schemaAssert, schemaXmldsig, schemaXenc, schemaProto]
+          });
+
+          if (!validationResult.errors) {
+            return resolve('SUCCESS_VALIDATE_XML');
+          }
+
+          console.error(`this is not a valid saml response with errors: ${validationResult.errors}`);
+          return reject('ERR_EXCEPTION_VALIDATE_XML');
+
         });
       }
     };
