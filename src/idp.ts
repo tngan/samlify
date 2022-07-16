e67e5b1fd07395de6bca7ebc007a04193cec5a30
tngan <tonynwk919@protonmail.com>
2022-07-16 21:51:10 +0800

    Prototype on new interface

---
+++ src/idp.ts
@@ -0,0 +1,43 @@
+/**
+ * Define the identity provider interface, construction and feature
+ * 
+ * Usage:
+ * 
+ * const idp = create(props);
+ * const sp = create(props);
+ * 
+ * // Perform the validation and pre-operation check before running into problem
+ * // during runtime
+ * 
+ * const app = bind(idp, sp);
+ * 
+ * app.createLoginRequest();
+ * app.createLogoutRequest();
+ * app.processLoginRequest();
+ * app.processLogoutRequest();
+ * 
+ */
+
+export type CreateProps = {
+  //
+  id: string;
+};
+
+export type BindOptions = {
+
+};
+
+export interface IdentityProvider {
+  id: string
+};
+
+/**
+ * Create function and returns a set of helper functions
+ */
+export const create = (props: CreateProps): IdentityProvider => {
+
+  return {
+    id: props.id
+  };
+
+}
